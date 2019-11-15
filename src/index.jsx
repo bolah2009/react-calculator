ef0861d82fad30b1036f1f9ea9e87bdede1ec8a1
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2019-11-15 15:06:19 +0100

    Add setup files

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const Calculator = () => (
+  <h1 className="main">Calculator App</h1>
+);
+
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
