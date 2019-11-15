8dac4f26d0347e4159a708af89f7f15c618d14cc
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2019-11-15 19:59:53 +0100

    Add the ButtonPanel component
    The ButtonPanel component divides the calculator panel into groups with divs.
    It accepts an array of labels (String) to be passed to the button component and an id.

---
+++ src/components/ButtonPanel.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
+import styles from '../css/modules/ButtonPanel.module.css';
+
+const buttonsLabel = [
+  { group: ['AC', '+/-', '%', '÷'], id: 'group-one' },
+  { group: ['7', '8', '9', 'X'], id: 'group-two' },
+  { group: ['4', '5', '6', '-'], id: 'group-three' },
+  { group: ['1', '2', '3', '+'], id: 'group-four' },
+  { group: ['0', '.', '='], id: 'group-five' },
+];
+
+const ButtonGroup = ({ group, id }) => {
+  const buttonGroups = group.map((label) => (
+    <Button key={label} name={label} isWide={label === '0'} />
+  ));
+  return (
+    <div className={`d-flex as-c ${styles['button-group']} ${id}`}>{buttonGroups}</div>
+  );
+};
+
+const ButtonPanel = () => (
+  buttonsLabel.map(({ group, id }) => (
+    <ButtonGroup key={id} group={group} id={id} />
+  ))
+);
+
+ButtonGroup.propTypes = {
+  group: PropTypes.arrayOf(PropTypes.string).isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+export default ButtonPanel;
